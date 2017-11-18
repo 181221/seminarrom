f1fdf5ad605a200f8cafca0cb2c33051c2aca61c
181192 <181192@stud.hvl.no>
2017-11-18 18:07:00 +0100

    Tokens
    Signed-off-by: 181192 <181192@stud.hvl.no>

--- seminarrom/src/main/java/no/pk/controller/SlackBotStart.java
+++ seminarrom/src/main/java/no/pk/controller/SlackBotStart.java
@@ -3,7 +3,7 @@ package no.pk.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "example.jbot"})
+@SpringBootApplication(scanBasePackages = {"no.pk.bot", "no.pk.controller"})
 public class SlackBotStart {
 
     /**
