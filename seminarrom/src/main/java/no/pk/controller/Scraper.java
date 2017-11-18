3271a22e601993cbb6e72dce2985e64006df6f38
181192 <181192@stud.hvl.no>
2017-11-18 19:25:42 +0100

    Env variabel på chrome

--- seminarrom/src/main/java/no/pk/controller/Scraper.java
+++ seminarrom/src/main/java/no/pk/controller/Scraper.java
@@ -19,7 +19,7 @@ public class Scraper {
     private static final String SUBJECT_LEDIGE_ROM = "Ledige rom: ";
     private static final String ALLESEMINAR = "https://no.timeedit.net/web/hib/db1/service/ri1AY6YYcnd8v5QYwYQrxgb1ZxgYxm98KaYravr5jY5awSadjc8vm5ZQ0Q522x60Yy5505YgX6g5Z5252Yg.html";
     private static final String LOGIN_FEIDE = "https://idp.feide.no/simplesaml/module.php/feide/login.php?asLen=196&AuthState=_9fca4163f7ea1def117e4e14f389d3a338a30db096%3Ahttps%3A%2F%2Fidp.feide.no%2Fsimplesaml%2Fsaml2%2Fidp%2FSSOService.php%3Fspentityid%3Durn%253Amace%253Afeide.no%253Aservices%253Ase.timeedit.hib%26cookieTime%3D1510783130%26RelayState%3D";
-    private static final String DRIVER_LOKAL = "D:\\Utvikling\\Ressurser\\Java\\chromedriver\\chromedriver.exe";
+    private static final String DRIVER_LOKAL = System.getenv("DRIVER_LOKAL");
 
     public static void main(String[] args) throws IOException {
         // setup driver.
