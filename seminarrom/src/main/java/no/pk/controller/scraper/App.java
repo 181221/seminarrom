b3ab76bb3ecd9eeb51429e433ed4f98341fb3ff3
pederDesktop <peder.wiig@gmail.com>
2017-11-19 23:48:59 +0100

    refak

--- seminarrom/src/main/java/no/pk/controller/scraper/App.java
+++ seminarrom/src/main/java/no/pk/controller/scraper/App.java
@@ -1,28 +1,21 @@
 package no.pk.controller.scraper;
 
-import no.pk.controller.Scraper;
 import no.pk.controller.attributter.Lenker;
-import no.pk.model.Rom;
-import no.pk.util.CsvReaderUtil;
 import no.pk.util.ReaderHjelp;
-import no.pk.util.RomUtil;
-import org.openqa.selenium.WebDriver;
-
 import java.io.IOException;
-import java.util.ArrayList;
-
 import static no.pk.util.CsvReaderUtil.readCSVInternett;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        //Scraper scraper = new Scraper();
-        //scraper.loggInnFeide();
-        //scraper.getDriver().navigate().to("https://no.timeedit.net/web/hib/db1/student/ri18840446X41YQ0X8Q82016Z011817XXY100Y851Y53431X8Y0YXXX5351Y1Y08431X1XY4YY8400Y0Y1180338XX08980901X8381xYY21Z5g85Z34W5002608X530yQ665Q5006.html");
-        //CsvReaderUtil.headlessReadcsv("asd");
+    /**
+     * Skriver ut alle FINN LEDIGE AUDITORIUM OG SEMINARROM KRONSTAD
+     * @throws IOException
+     */
+    public static String hentDagensSeminarogAuditorieRom() throws IOException {
         ReaderHjelp reader = readCSVInternett(Lenker.SEMINAR_AUDITORIUM);
         reader.finnAlleLedige();
         String melding = reader.lagMsg();
-        System.out.println(melding);
+        return melding;
     }
+
 
 }
