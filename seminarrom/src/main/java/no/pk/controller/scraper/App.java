29f33efd69c8109400ee56a4d7ae8e4d09c874d2
pederDesktop <peder.wiig@gmail.com>
2017-11-20 00:06:41 +0100

    Lagt til switch på hvilken type man vil skrive inn

--- seminarrom/src/main/java/no/pk/controller/scraper/App.java
+++ seminarrom/src/main/java/no/pk/controller/scraper/App.java
@@ -2,7 +2,11 @@ package no.pk.controller.scraper;
 
 import no.pk.controller.attributter.Lenker;
 import no.pk.util.ReaderHjelp;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static no.pk.util.CsvReaderUtil.readCSVInternett;
 
 public class App {
@@ -10,10 +14,22 @@ public class App {
      * Skriver ut alle FINN LEDIGE AUDITORIUM OG SEMINARROM KRONSTAD
      * @throws IOException
      */
-    public static String hentDagensSeminarogAuditorieRom() throws IOException {
+    public static String hentLedigDagensSeminarogAuditorieRom(String type) throws IOException {
         ReaderHjelp reader = readCSVInternett(Lenker.SEMINAR_AUDITORIUM);
-        reader.finnAlleLedige();
-        String melding = reader.lagMsg();
+        String melding = "";
+        switch (type) {
+            case "ledignaa":
+                melding = reader.LedigNaa();
+                break;
+            case "ledige":
+                melding = Arrays.toString(new ArrayList[]{reader.getLedigerom()});
+                break;
+            case "allerom":
+                melding = Arrays.toString(new ArrayList[]{reader.getAllerom()});
+                break;
+            default:
+                melding = "Skriv inn en av tre flølgene kommandoer: \"ledignaa\" \"ledige\" \"allerom\"";
+        }
         return melding;
     }
 
